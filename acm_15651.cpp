ea5e6532c41d397121c1bf00f81f977c0fcfbff4
Gyeonghwan Lee <rudghks1108@naver.com>
2019-10-02 11:32:58 +0900

    15651번: N과 M(3)

---
+++ acm_15651.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> intVec;
+
+void printNM(int index, int N, int M)
+{
+    if (index == M)
+    {
+        for (int i = 0; i < intVec.size(); i++)
+        {
+            cout << intVec[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+
+    for (int i = 1; i <= N; i++)
+    {
+
+        intVec.push_back(i);
+        printNM(index + 1, N, M);
+        intVec.pop_back();
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    int N, M;
+    cin >> N >> M;
+    printNM(0, N, M);
+
+    return 0;
+}
