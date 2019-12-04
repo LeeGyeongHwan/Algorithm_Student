34deb5845b34b050e36875fc8ae58b81f3d4effe
Gyeonghwan Lee <rudghks1108@naver.com>
2019-12-04 10:06:00 +0900

    2444번: 별 찍기 - 7

---
+++ acm_2444.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int starNum;
+    cin >> starNum;
+
+    for (int a = 0; a < starNum - 1; a++)
+    {
+        for(int i=0;i<starNum-a-1;i++)
+            cout<<" ";
+        for(int i=0;i<(2*a+1);i++)
+            cout<<"*";
+
+        cout << endl;
+    }
+
+    for (int a = 0; a < starNum; a++)
+    {
+        for (int j = 0; j < a; j++)
+            cout << " ";
+        for (int i = 0; i < starNum * 2 - (a * 2 + 1); i++)
+            cout << "*";
+
+        cout << endl;
+    }
+
+    return 0;
+}
