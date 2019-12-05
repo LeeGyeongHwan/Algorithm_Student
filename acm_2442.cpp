d3378dac3fcfe498012583440d5e5ada5bffeb0c
Gyeonghwan Lee <rudghks1108@naver.com>
2019-12-05 10:54:29 +0900

    2442번: 별 찍기 - 5

---
+++ acm_2442.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int starNum;
+    cin >> starNum;
+
+    for (int a = 0; a < starNum; a++)
+    {
+        for(int i=0;i<starNum-a-1;i++)
+            cout<<" ";
+        for(int i=0;i<(2*a+1);i++)
+            cout<<"*";
+
+        cout << endl;
+    }
+
+
+    return 0;
+}
