7bb22ac124f4833a8a8f9d6b1f1d88de63999f48
Gyeonghwan Lee <rudghks1108@naver.com>
2019-12-24 12:35:17 +0900

    2523번: 별 찍기 - 13

---
+++ acm_2523.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = i + 1; j > 0; j--)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for (int i = 0; i < N - 1; i++)
+    {
+        for (int j = 0; j < N - (i + 1); j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
