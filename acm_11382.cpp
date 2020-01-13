85214a1ef0c8bf0196c9f0b66381cce9f6fa9c43
Gyeonghwan Lee <rudghks1108@naver.com>
2020-01-13 21:31:56 +0900

    11382번: 꼬마 정민

---
+++ acm_11382.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    long long A, B, C;
+
+    cin>>A>>B>>C;
+
+    long long sumLong = A+B+C;
+
+    cout<<sumLong<<endl;
+
+    return 0;
+}
