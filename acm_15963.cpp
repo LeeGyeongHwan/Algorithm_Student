c43396a8468432f89b2dc1e8fe2dca74c0ad4996
Gyeonghwan Lee <rudghks1108@naver.com>
2020-01-14 20:56:17 +0900

    15963번: CASIO

---
+++ acm_15963.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long N, M;
+
+    cin >> N >> M;
+
+    if (N == M)
+        cout << 1 << endl;
+    else
+        cout << 0 << endl;
+
+    return 0;
+}
