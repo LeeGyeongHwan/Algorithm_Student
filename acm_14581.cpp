5a3d0a393e84f8d30ad7f35dc30ece537ab3a38f
reamer <rudghks1108@naver.com>
2020-01-27 10:32:36 +0900

    14581번: 팬들에게 둘러싸인 홍준

---
+++ acm_14581.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+using  namespace std;
+
+int main()
+{
+    string str;
+    cin>>str;
+
+    cout<<":fan::fan::fan:"<<endl;
+    cout<<":fan::"<<str<<"::fan:"<<endl;
+    cout<<":fan::fan::fan:"<<endl;
+
+    return 0;
+}
