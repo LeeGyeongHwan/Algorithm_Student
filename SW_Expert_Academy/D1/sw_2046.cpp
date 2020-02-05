130f28841992facd587233cacc556a0b778e902e
Unknown <rudghks1108@naver.com>
2020-02-05 09:09:09 +0900

    SW_D1_2046: 스탬프 찍기

---
+++ SW_Expert_Academy/D1/sw_2046.cpp
@@ -0,0 +1,24 @@
+/*
+난이도 : D1
+문제 번호 : 2046
+문제 제목 : 스탬프 찍기
+풀이 날짜 : 2020-02-05
+Solved By Reamer
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+    cin >> num;
+    for (int i = 0; i < num; i++)
+    {
+        cout << "#";
+    }
+    cout << endl;
+
+    return 0;
+}
