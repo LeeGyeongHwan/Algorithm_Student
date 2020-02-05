6c2392c90b65962aa3c58fd6a0bfea3990f78ce0
reamer <rudghks1108@naver.com>
2020-02-06 00:18:59 +0900

    SW_D1_2043: 서랍의 비밀번호

---
+++ SW_Expert_Academy/D1/sw_2043.cpp
@@ -0,0 +1,21 @@
+/*
+난이도 : D1
+문제 번호 : 2043
+문제 제목 : 서랍의 비밀번호
+풀이 날짜 : 2020-02-06
+Solved By Reamer
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int P, K;
+
+    cin >> P >> K;
+    cout << P - K + 1 << endl;
+
+    return 0;
+}
