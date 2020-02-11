da6dd86e7f2d551ef4cae8e88f15298d5e243ecf
Unknown <rudghks1108@naver.com>
2020-02-11 09:29:50 +0900

    SW_D1_2027: 대각선 출력하기

---
+++ SW_Expert_Academy/D1/sw_2027.cpp
@@ -0,0 +1,17 @@
+/*
+난이도 : D1
+문제 번호 : 2027
+문제 제목 : 대각선 출력하기
+풀이 날짜 : 2020-02-11
+Solved By Reamer
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "#++++\n+#+++\n++#++\n+++#+\n++++#"<<endl;
+    return 0;
+}
