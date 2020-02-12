8a3853ed9ab21aa116afc08db70bd9287aa2bba9
Unknown <rudghks1108@naver.com>
2020-02-13 08:50:17 +0900

    SW_D1_2025: N줄덧셈

---
+++ SW_Expert_Academy/D1/sw_2025.cpp
@@ -0,0 +1,19 @@
+/*
+난이도 : D1
+문제 번호 : 2025
+문제 제목 : N줄덧셈
+풀이 날짜 : 2020-02-13
+Solved By Reamer
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+    cin >> num;
+    cout << num * (num + 1) / 2 << endl;
+    return 0;
+}
