b13d0960a91a46e9f39f73e128f971ea63198d7f
Unknown <rudghks1108@naver.com>
2020-02-14 08:59:15 +0900

    SW_D1_1938: 아주 간단한 계산기

---
+++ SW_Expert_Academy/D1/sw_1938.cpp
@@ -0,0 +1,22 @@
+/*
+난이도 : D1
+문제 번호 : 1938
+문제 제목 : 아주 간단한 계산기
+풀이 날짜 : 2020-02-14
+Solved By Reamer
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int numA, numB;
+    cin >> numA >> numB;
+    cout << numA + numB << endl;
+    cout << numA - numB << endl;
+    cout << numA * numB << endl;
+    cout << numA / numB << endl;
+    return 0;
+}
