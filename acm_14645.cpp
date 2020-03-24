1b90afec68424cdbab4c109179f21571aa490e88
desktop <rudghks1108@naver.com>
2020-03-24 20:56:29 +0900

    14645번: 와이버스 부릉부릉

---
+++ acm_14645.cpp
@@ -0,0 +1,23 @@
+/*
+ACMICPC
+문제 번호 : 14645
+문제 제목 : 와이버스 부릉부릉
+풀이 날짜 : 2020-03-24
+Solved By Reamer
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int N, K, A, B;
+    cin >> N >> K;
+    for (int i = 0; i < N; i++)
+    {
+        cin >> A >> B;
+    }
+    cout << "비와이" << endl;
+    return 0;
+}
