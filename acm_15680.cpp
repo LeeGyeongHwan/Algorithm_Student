e25dc6f7cd2db7da75cfbbc0b43284095771b724
Gram <rudghks1108@naver.com>
2020-03-26 16:49:31 +0900

    15680번: 연세대학교

---
+++ acm_15680.cpp
@@ -0,0 +1,21 @@
+/*
+ACMICPC
+문제 번호 : 15680
+문제 제목 : 연세대학교
+풀이 날짜 : 2020-03-26
+Solved By Reamer
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cin >> num;
+    if (num == 0)
+        cout << "YONSEI" << endl;
+    else
+        cout << "Leading the Way to the Future" << endl;
+    return 0;
+}
