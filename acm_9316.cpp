bfa431db37a044c7ab1b039375d3b8696f8b2928
Gram <rudghks1108@naver.com>
2020-03-30 15:44:34 +0900

    9316번: Hello Judge

---
+++ acm_9316.cpp
@@ -0,0 +1,19 @@
+/*
+ACMICPC
+문제 번호 : 9316
+문제 제목 : Hello Judge
+풀이 날짜 : 2020-03-30
+Solved By Reamer
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cin >> num;
+    for (int i = 1; i <= num; i++)
+        cout << "Hello World, Judge " << i << "!\n";
+    return 0;
+}
