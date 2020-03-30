8232d080ead0218179432f2a91578c70915734a1
Gram <rudghks1108@naver.com>
2020-03-30 15:48:08 +0900

    9653번: 스타워즈 로고

---
+++ acm_9653.cpp
@@ -0,0 +1,26 @@
+/*
+ACMICPC
+문제 번호 : 9653
+문제 제목 : 스타워즈 로고
+풀이 날짜 : 2020-03-30
+Solved By Reamer
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"    8888888888  888    88888"<<endl;
+    cout<<"   88     88   88 88   88  88"<<endl;
+    cout<<"    8888  88  88   88  88888"<<endl;
+    cout<<"       88 88 888888888 88   88"<<endl;
+    cout<<"88888888  88 88     88 88    888888"<<endl;
+    cout<<endl;
+    cout<<"88  88  88   888    88888    888888"<<endl;
+    cout<<"88  88  88  88 88   88  88  88"<<endl;
+    cout<<"88 8888 88 88   88  88888    8888"<<endl;
+    cout<<" 888  888 888888888 88  88      88"<<endl;
+    cout<<"  88  88  88     88 88   88888888"<<endl;
+    return 0;
+}
