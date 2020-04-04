0ee1e608d287747354d52d1d50b4967697b71598
desktop <rudghks1108@naver.com>
2020-04-04 22:10:52 +0900

    15962번: 새로운 시작

---
+++ acm_15962.cpp
@@ -0,0 +1,16 @@
+/*
+ACMICPC
+문제 번호 : 15962
+문제 제목 : 새로운 시작
+풀이 날짜 : 2020-04-04
+Solved By Reamer
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "파이팅!!" << endl;
+    return 0;
+}
