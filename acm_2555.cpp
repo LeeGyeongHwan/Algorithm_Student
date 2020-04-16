404a02733b9f722defc197992fba7578c4ec8f0f
desktop <rudghks1108@naver.com>
2020-04-16 20:31:42 +0900

    2555번: 생일 출력하기

---
+++ acm_2555.cpp
@@ -0,0 +1,16 @@
+/*
+ACMICPC
+문제 번호 : 2555
+문제 제목 : 생일 출력하기
+풀이 날짜 : 2020-04-16
+Solved By Reamer
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "10/14" << endl;
+    return 0;
+}
