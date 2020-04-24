7f5922dcb38ba7302863647e08f074b9226e3080
Gram <rudghks1108@naver.com>
2020-04-24 16:12:07 +0900

    15439번: Vera and Outfits

---
+++ acm_15439.cpp
@@ -0,0 +1,19 @@
+/* 
+ACMICPC 
+문제 번호 : 15439 
+문제 제목 : Vera and Outfits
+풀이 날짜 : 2020-04-24
+Solved By Reamer 
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+    cin >> num;
+    cout << num * num - num << endl;
+    return 0;
+}
