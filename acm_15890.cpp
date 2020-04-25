0702251b3862a185c6e6cf6efabf55803bdd8c44
desktop <rudghks1108@naver.com>
2020-04-25 16:24:01 +0900

    15890번: 전국 대학생 프로그래밍 대회 동아리 연합 여름 대회 2018

---
+++ acm_15890.cpp
@@ -0,0 +1,17 @@
+/*
+ACMICPC
+문제 번호 : 15890
+문제 제목 : 전국 대학생 프로그래밍 대회 동아리 연합 여름 대회 2018
+풀이 날짜 : 2020-04-25
+Solved By Reamer
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "20180729" << endl;
+    return 0;
+}
