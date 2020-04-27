b3bd68536add6d5f4ac2a91631464248fdb5b538
desktop <rudghks1108@naver.com>
2020-04-27 22:41:29 +0900

    9654번: 나부 함대 데이터

---
+++ acm_9654.cpp
@@ -0,0 +1,23 @@
+/*
+ACMICPC
+문제 번호 : 9654
+문제 제목 : 나부 함대 데이터
+풀이 날짜 : 2020-04-27
+Solved By Reamer
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    cout << "SHIP NAME      CLASS          DEPLOYMENT IN SERVICE" << endl;
+    cout << "N2 Bomber      Heavy Fighter  Limited    21        " << endl;
+    cout << "J-Type 327     Light Combat   Unlimited  1         " << endl;
+    cout << "NX Cruiser     Medium Fighter Limited    18        " << endl;
+    cout << "N1 Starfighter Medium Fighter Unlimited  25        " << endl;
+    cout << "Royal Cruiser  Light Combat   Limited    4         " << endl;
+    return 0;
+}
