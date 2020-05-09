85f04275d41209378850fc14ba1ae0369a176dd2
desktop <rudghks1108@naver.com>
2020-05-09 18:09:05 +0900

    5338번: 마이크로소프트 로고

---
+++ acm_5338.cpp
@@ -0,0 +1,21 @@
+/* 
+ACMICPC 
+문제 번호 : 5338
+문제 제목 : 마이크로소프트 로고
+풀이 날짜 : 2020-05-09
+Solved By Reamer 
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "       _.-;;-._" << endl;
+    cout << "'-..-'|   ||   |" << endl;
+    cout << "'-..-'|_.-;;-._|" << endl;
+    cout << "'-..-'|   ||   |" << endl;
+    cout << "'-..-'|_.-''-._|" << endl;
+    return 0;
+}
