84c426a0a3d187e791515f6f8c6e9f97717f75f1
desktop <rudghks1108@naver.com>
2020-05-10 20:31:14 +0900

    5337번: 웰컴

---
+++ acm_5337.cpp
@@ -0,0 +1,19 @@
+/* 
+ACMICPC 
+문제 번호 : 5337
+문제 제목 : 웰컴 
+풀이 날짜 : 2020-05-10
+Solved By Reamer 
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << ".  .   ." << endl;
+    cout << "|  | _ | _. _ ._ _  _" << endl;
+    cout << "|/\\|(/.|(_.(_)[ | )(/." << endl;
+    return 0;
+}
