e66c8cf80dcc6e9897d9de8ffc595458530b093e
Gram <rudghks1108@naver.com>
2020-06-26 09:33:05 +0900

    5539번: 콜센터

---
+++ acm_5339.cpp
@@ -0,0 +1,27 @@
+/* 
+ACMICPC 
+문제 번호 : 5339
+문제 제목 : 콜센터
+풀이 날짜 : 2020-06-26
+Solved By Reamer 
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"     /~\\"<<endl;
+    cout<<"    ( oo|"<<endl;
+    cout<<"    _\\=/_"<<endl;
+    cout<<"   /  _  \\"<<endl;
+    cout<<"  //|/.\\|\\\\"<<endl;
+    cout<<" ||  \\ /  ||"<<endl;
+    cout<<"============"<<endl;
+    cout<<"|          |"<<endl;
+    cout<<"|          |"<<endl;
+    cout<<"|          |"<<endl;
+    
+    return 0;
+}
