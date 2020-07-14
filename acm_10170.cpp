14cf2f7e738a90dac5c19f543f3d961eedc4e905
Gram <rudghks1108@naver.com>
2020-07-14 21:44:30 +0900

    10170번: NFC West vs North

---
+++ acm_10170.cpp
@@ -0,0 +1,28 @@
+/*
+ACMICPC
+문제 번호 : 10170
+문제 제목 : NFC West vs North
+풀이 날짜 : 2020-07-14
+Solved By Reamer
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "NFC West       W   L  T" << endl;
+    cout << "-----------------------" << endl;
+    cout << "Seattle        13  3  0" << endl;
+    cout << "San Francisco  12  4  0" << endl;
+    cout << "Arizona        10  6  0" << endl;
+    cout << "St. Louis      7   9  0" << endl;
+    cout << endl;
+    cout << "NFC North      W   L  T" << endl;
+    cout << "-----------------------" << endl;
+    cout << "Green Bay      8   7  1" << endl;
+    cout << "Chicago        8   8  0" << endl;
+    cout << "Detroit        7   9  0" << endl;
+    cout << "Minnesota      5  10  1" << endl;
+    return 0;
+}
