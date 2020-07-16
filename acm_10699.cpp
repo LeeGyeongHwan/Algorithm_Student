abfe3a6b262059e51e2fb011681cc941711371bf
Gram <rudghks1108@naver.com>
2020-07-16 19:08:41 +0900

    10699번: 오늘 날짜

---
+++ acm_10699.cpp
@@ -0,0 +1,16 @@
+/*
+ACMICPC
+문제 번호 : 10699
+문제 제목 : 오늘 날짜
+풀이 날짜 : 2020-07-16
+Solved By Reamer
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "2020-07-16" << endl;
+    return 0;
+}
