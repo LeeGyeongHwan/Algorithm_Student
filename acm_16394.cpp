a8b9e9e514146619d082fb5fd7f6cb7defa74084
Gram <rudghks1108@naver.com>
2020-07-17 08:50:02 +0900

    16394번: 홍익대학교

---
+++ acm_16394.cpp
@@ -0,0 +1,21 @@
+/*
+ACMICPC
+문제 번호 : 16394
+문제 제목 : 홍익대학교
+풀이 날짜 : 2020-07-17
+Solved By Reamer
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int year;
+
+    cin >> year;
+
+    cout << year - 1946 << endl;
+
+    return 0;
+}
