68f0a3c80274e0df4b486cf469b86495feef27ae
Gram <rudghks1108@naver.com>
2020-07-30 09:25:25 +0900

    16170번: 오늘의 날짜는?

---
+++ acm_16170.cpp
@@ -0,0 +1,18 @@
+/*
+ACMICPC
+문제 번호 : 16170
+문제 제목 : 오늘의 날짜는?
+풀이 날짜 : 2020-07-30
+Solved By Reamer
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "2020\n07\n30";
+
+    return 0;
+}
